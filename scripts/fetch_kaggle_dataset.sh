65808deddb26d1657b7086fa0fa4f344333135b6
Kalinin Andrey <andreyypnz@yandex.ru>
2021-04-06 17:45:31 +0300

    added package3

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-kaggle competitions download -c house-prices-advanced-regression-techniques -p packages/regression_model/regression_model/datasets/
-ls
+kaggle competitions download -c house-prices-advanced-regression-techniques -p ../packages/regression_model/regression_model/datasets/
+unzip ../packages/regression_model/regression_model/datasets/house-prices-advanced-regression-techniques.zip -d ../packages/regression_model/regression_model/datasets/
+ls ../packages/regression_model/regression_model/datasets/
